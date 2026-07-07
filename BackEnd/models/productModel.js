8bca321768d11137a2b56094a96807e253ade440
Rehman Banday <rehmanbanday5@gmail.com>
2026-07-07 16:03:48 +0500

    Update admin auth and product controller

--- BackEnd/models/productModel.js
+++ BackEnd/models/productModel.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema({
   subCategory: { type: String, required: true },
   sizes: { type: Array, required: true },
   bestseller: { type: Boolean },
-  date: { type: number, required: true },
+  date: { type: Number, required: true },
 });
 
 const productModel = mongoose.models.product || mongoose.model("product", productSchema);
