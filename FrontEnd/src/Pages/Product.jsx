ef4af27163f7fba44df6f2a23cf43a35656a156a
Rehman Banday <rehmanbanday5@gmail.com>
2026-07-14 16:00:49 +0500

    Done

--- FrontEnd/src/Pages/Product.jsx
+++ FrontEnd/src/Pages/Product.jsx
@@ -37,7 +37,7 @@ const Product = () => {
      return productData ? (
        <div className="border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100">
          {/* ------------------ Product Data ------------------------ */}
-         <div className="flex gap-12 sm:gap-12 flex-colsm:flex-row">
+         <div className="flex gap-12 sm:gap-12 flex-col sm:flex-row">
            {/* ----------------- Product Images ------------------ */}
            <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
              <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full">
