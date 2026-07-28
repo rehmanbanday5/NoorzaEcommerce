1d207e7d9ee1041a77f90d6d704140750453f800
Rehman Banday <rehmanbanday5@gmail.com>
2026-07-28 19:42:21 +0500

    Noorza Website

--- BackEnd/config/mongodb.js
+++ BackEnd/config/mongodb.js
@@ -1,18 +1,17 @@
+
 import mongoose from 'mongoose';
 
-const connectDB = async () =>{
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('connected', () => {
+            console.log('DB Connected');
+        });
 
-    mongoose.connection.on('connected',() => {
-        console.log("DB Connected");
+        await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
 
-    })
-
-
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
-
-
-} 
-
+    } catch (error) {
+        console.log('MongoDB Connection Error:', error);
+    }
+};
 
 export default connectDB;
