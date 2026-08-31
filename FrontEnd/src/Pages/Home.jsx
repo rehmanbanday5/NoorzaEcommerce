50a4fd43844cc78b46e0973e1259f78a10acacff
Rehman Banday <rehmanbanday5@gmail.com>
2026-08-31 18:40:40 +0500

    Noorza

--- FrontEnd/src/Pages/Home.jsx
+++ FrontEnd/src/Pages/Home.jsx
@@ -1,24 +1,31 @@
-import Hero from '../Components/Hero'
-import LatestCollection from '../Components/LatestCollection'
-import BestSeller from '../Components/BestSeller'
+import { lazy, Suspense } from "react";
+
+import Hero from "../Components/Hero";
 import ShopByCategory from "../Components/ShopByCategory";
-import WhyChooseNoorza from "../Components/WhyChooseNoorza";
-import InstagramSection from "../Components/InstagramSection";
 
+const LatestCollection = lazy(() => import("../Components/LatestCollection"));
 
+const WhyChooseNoorza = lazy(() => import("../Components/WhyChooseNoorza"));
 
+const BestSeller = lazy(() => import("../Components/BestSeller"));
+
+const InstagramSection = lazy(() => import("../Components/InstagramSection"));
 
 const Home = () => {
   return (
     <div>
       <Hero />
+
       <ShopByCategory />
-      <LatestCollection />
-      <WhyChooseNoorza />
-      <BestSeller />
-      <InstagramSection />
+
+      <Suspense fallback={null}>
+        <LatestCollection />
+        <WhyChooseNoorza />
+        <BestSeller />
+        <InstagramSection />
+      </Suspense>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
